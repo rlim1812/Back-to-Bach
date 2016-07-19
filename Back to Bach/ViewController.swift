d14fe49eaf1fac7acb35e703ff378d1297e54f50
Ryan Lim <rlim1812@gmail.com>
2016-07-19 10:43:43 -0700

    This is the first version of the app

--- Back to Bach/ViewController.swift
+++ Back to Bach/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    @IBAction func play(sender: AnyObject) {
+        player.play()
+    }
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+    
+    @IBOutlet weak var scrubSlider: UISlider!
+    
+    @IBAction func scrub(sender: AnyObject) {
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        player.pause()
+    }
+    @IBAction func stop(sender: AnyObject) {
+        player.pause()
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!))
+        } catch {
+            //It didn't work!
+        }
+    }
+    
+    func updateScrubSlider(){
+        scrubSlider.value = Float(player.currentTime)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!))
+            scrubSlider.maximumValue = Float(player.duration)
+        } catch {
+            //It didn't work!
+        }
+        
+         _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateScrubSlider", userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
